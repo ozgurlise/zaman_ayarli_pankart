7efa1eac9099155a6f28b061c8f8c1ea4f7a7603
ÖZGÜR LİSE <55161115+ozgurlise@users.noreply.github.com>
2019-11-14 23:49:37 +0300

    Update zaman_ayarli_kod.ino

--- zaman_ayarli_kod.ino
+++ zaman_ayarli_kod.ino
@@ -5,22 +5,23 @@ Servo servo;
 unsigned long yeniZaman =0;
 unsigned long sure_ms;
 unsigned long dakika;
-void setup() {
-  servo.attach(9); 
+void setup() 
+{
+    servo.attach(9); 
 }
 
-void loop() {
-  
-  
-  dakika=30;// SADECE DAKİKAYİ GİRİN
+void loop() 
+{
+    dakika=30;// SADECE DAKİKAYİ GİRİN
 
  
-  sure_ms=((dakika*60)*1000);
-  yeniZaman = millis();  
-  if(yeniZaman>sure_ms){
-  servo.write(0);
-  delay(400);
-  servo.write(180);
-  delay(400); 
-  }
+    sure_ms=((dakika*60)*1000);
+    yeniZaman = millis();  
+    if(yeniZaman>sure_ms)
+    {
+        servo.write(0);
+        delay(400);
+        servo.write(180);
+        delay(400); 
+    }
 }
